36c58ce807bde561d586141f8a4e3883edfffd6c
agracio <antonas.graciovas@outlook.com>
2017-04-16 23:54:09 +0100

    .Net Core build fixes

--- tools/test.js
+++ tools/test.js
@@ -33,7 +33,7 @@ else {
 
 function runOnSuccess(code, signal) {
 	if (code === 0) {
-		process.env['EDGE_APP_ROOT'] = path.join(testDir, 'bin', 'Debug', 'netcoreapp1.0');
+		process.env['EDGE_APP_ROOT'] = path.join(testDir, 'bin', 'Debug', 'netstandard1.6');
 
 		spawn('node', [mocha, testDir, '-R', 'spec', '-t', '10000', '-gc'], { 
 			stdio: 'inherit' 
